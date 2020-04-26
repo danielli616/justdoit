113a6e4590baa49d9a811951399b77dbeb28bb64
yzhang <yzhang@fast-lta.de>
2020-04-26 17:09:29 +0200

    make it concise

--- 509_Fibonacci_Number/fibonaccinumber.cpp
+++ 509_Fibonacci_Number/fibonaccinumber.cpp
@@ -1,30 +1,19 @@
 class Solution {
-public:
-	int fib(int N) {
+    public:
+        int fib(int N) {
+            return memorize(N);
+        }
 
-		if (N <= 1) {
-			return N;
-		}
-		return memoize(N);
-	}
-	int memoize(int N) {
-		std::vector<int> nums(N);
+        int memorize(int N) {
+            if (N <= 1)
+                return N;
+            std::vector<int> nums(N+1);
 
-		for (int i = 0; i < N; i++)
-		{
-			if (i == 0)
-			{
-				nums.at(i) = 0;
-			}
-			else if (i == 1)
-			{
-				nums.at(i) = 1;
-			}
-			else
-			{
-				nums.at(i) = nums.at(i - 1) + nums.at(i - 2);
-			}
-		}
-		return  nums.at(N - 1) + nums.at(N - 2);
-	}
+            nums[1] = 1;
+            for (int i = 2 ; i < N+1 ; i++)
+            {
+                nums[i] =   nums[i - 1] +  nums[i - 2];
+            }
+            return nums[N];
+        }
 };
