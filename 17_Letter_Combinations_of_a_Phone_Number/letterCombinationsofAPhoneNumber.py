63aa551f5fa139527b548d4578207c39d3d6980e
Daniel Li <liyongjiang616@gmail.com>
2020-07-19 01:20:32 +0200

    I am back 17. Letter Combinations of a Phone Number

---
+++ 17_Letter_Combinations_of_a_Phone_Number/letterCombinationsofAPhoneNumber.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}        
+    
+        output = []
+        if digits: 
+            self.dfs("", digits, output, phone)
+        
+        return output
+    
+    
+    def dfs(self, combination, next_digits, output, phone):
+        if len(next_digits) == 0:
+            output.append(combination)
+        else:
+            for letter in phone[next_digits[0]]:
+                self.dfs(combination + letter, next_digits[1:], output, phone)
+            
+        
+        
